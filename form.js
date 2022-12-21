771422624c515840b5bc630c8b44f6da08db3a7f
ByGian26 <116897910+ByGian26@users.noreply.github.com>
2022-12-21 07:56:06 -0500

    Add files via upload

---
+++ form.js
@@ -0,0 +1,22 @@
+let app = angular.module('calcularApp', []);
+
+app.controller('calcularCtrl', 
+function($scope) {
+  $scope.distancia = '';
+  $scope.CalcularTotal = function() {
+    if ($scope.distancia) {
+      if ($scope.distancia <= 300) {
+            $scope.precio = 250;
+        }else if ($scope.distancia > 300 && $scope.distancia <= 1000) {
+            $scope.precio = ( 30*($scope.distancia - 300) )+250;
+        }else {
+            $scope.precio=  ((($scope.distancia - 1000) * 20) + (700 * 30) + 250 );
+        }
+
+      // Descuento 
+      if ($scope.precio > 500) {
+            $scope.precio = $scope.precio- ($scope.precio * 0.1);
+        }
+    }
+  }
+});
